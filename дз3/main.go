e5185cce6f44365782ee84ea1ad5a6f839a47649
amanjol98 <amanjols98@gmail.com>
2026-03-05 16:02:40 +0500

    Add files via upload

---
+++ дз3/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	// age := 28
+	// fmt.Println("Возраст", age)
+
+	// age = age + 1
+	// fmt.Println("Измененный возраст", age)
+
+	// height := 180
+	// fmt.Println("Рост", height, "сантиметров")
+
+	// height_in_meters := 1.8
+	// fmt.Println("Рост", height_in_meters, "метров")
+
+	// isStudent := true
+	// fmt.Println(isStudent)
+
+	// temperature := 7
+	// temperature = 33
+	// fmt.Println("Температура на улице сейчас =", temperature)
+
+	// if temperature < 25 {
+	// 	fmt.Println("Погода холодная")
+	// } else {
+	// 	fmt.Println("Погода теплая")
+	// }
+
+	// var favoriteQuote string = "Как сказал один мудрец ..."
+	// fmt.Println(favoriteQuote)
+
+	PI := 3.14
+	fmt.Println("Число пи равно = ", PI)
+
+	// PI = "31415"
+	// fmt.Println("Число пи равно = ", PI)
+	// Мы не сможем преобразавать значение переменной из типа данных float на string без использование strconv, так как в голанг типы данных всегда явные
+
+}
